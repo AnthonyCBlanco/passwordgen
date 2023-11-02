37076456c28fe5c79691843473a9a90fbff3e387
Anthony Blanco <anthony200527@outlook.com>
2023-11-02 12:38:07 -0700

    added lowercase uppercase var for pass word gen

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//
+function generatePassword(){
+  console.log("button pressed")
+  var charcters = []
+  var length = prompt("How Many Charcters Would You Like Your Password To Contain")
+  var lowercase = confirm("Press 'ok' if you would like lower case letters")
+  var uppercase = confirm("Press 'ok' if you would like Upper case letters")
+  var number = confirm("Press 'ok' if you would like Numbers")
+  var speialchar = confirm("Press 'ok' if you would like Special Characters")
+
+  if (between(length, 8, 128)){
+    if (lowercase != null){
+      charcters.push("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+    }
+    if(uppercase != null){
+      charcters.push("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+    }
+
+  }
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
